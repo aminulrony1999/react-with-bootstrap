7b9425c15f461a5c68a6590a43fc19f0e954b23f
aminulrony1999 <aminulrony1999@gmail.com>
2023-10-06 18:32:30 +0600

    card added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Button } from 'react-bootstrap'
+import { Button, Card } from 'react-bootstrap'
 import './App.css'
 
 function App() {
@@ -13,6 +13,18 @@ function App() {
       <Button variant="light">Light</Button>{' '}
       <Button variant="dark">Dark</Button>
       <Button variant="link">Link</Button>
+
+      <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
     </div>
   )
 }
