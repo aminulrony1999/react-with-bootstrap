1024577bd44fb4e1ca4deb4f6497eec57cd609f7
aminulrony1999 <aminulrony1999@gmail.com>
2023-10-07 13:47:19 +0600

    toggle bar added

--- src/components/Link/Link.jsx
+++ src/components/Link/Link.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Link.css';
-// eslint-disable-next-line react/prop-types
 const Link = (props) => {
     const {route} = props;
     return (
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,16 @@
-// eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react';
 import Link from '../Link/Link';
-import { Bars3Icon } from '@heroicons/react/24/solid'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
 const Navbar = () => {
     const [open,setOpen] = useState(false);
     const routes = [{id : 1, name : 'Home', path : '/home'}, {id : 2, name :'About', path:'/about'},{id : 3, name : 'Contact', path :'/contact'},{id : 4, name : 'Products', path : '/products'},{id : 5, name : 'Services', path : '/services'}]
     return (
         <nav>
             <div onClick={() => setOpen(!open)}>
-                <span>{open === true ? 'open' : 'close'}</span>
-                <Bars3Icon className="h-6 w-6 text-blue-500" />
+                <span>{open === true ? 
+                <XMarkIcon className="h-6 w-6 text-purple-500" /> 
+                : <Bars3Icon className="h-6 w-6 text-purple-500" />}
+                </span>
             </div>
             <ul className='md:flex'>
                 {
