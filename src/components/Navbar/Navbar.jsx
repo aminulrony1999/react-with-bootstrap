338aec50f3e06d5a1617d318b74daafb4fb5c6a0
aminulrony1999 <aminulrony1999@gmail.com>
2023-10-07 11:40:44 +0600

    Link added as component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import Link from '../Link/Link';
 
 const Navbar = () => {
     const routes = [{id : 1, name : 'Home', path : '/'}, {id : 2, name :'About', path:'/about'},{id : 3, name : 'Contact', path :'/contact'},{id : 4, name : 'Products', path : '/products'},{id : 5, name : 'Services', path : '/services'}]
     return (
         <nav>
-            <ul>
+            <ul className='md:flex'>
                 {
-                    routes.map(route => <li>{route.name}</li>)
+                    routes.map(route => <Link key = {route.id}> </Link>)
                 }
             </ul>
         </nav>
