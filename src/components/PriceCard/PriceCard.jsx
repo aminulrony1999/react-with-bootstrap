3a69f20ff98d31cb61fa8552d933bb3de4a32976
aminulrony1999 <aminulrony1999@gmail.com>
2023-10-07 19:01:18 +0600

    back ground color changed

--- src/components/PriceCard/PriceCard.jsx
+++ src/components/PriceCard/PriceCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './PriceCard.css';
 const PriceCard = ({price}) => {
     return (
-        <div>
+        <div className='bg-indigo-400'>
             <h2>
             <span className='text-purple-500 text-5xl font-extrabold'>{price.price}</span>
             <span className='text-2xl font-bold'>/mon</span>
