ca732b365186d2b794c1c2a9286488411ec6c1ea
aminulrony1999 <aminulrony1999@gmail.com>
2023-10-07 18:59:08 +0600

    font bolded

--- src/components/PriceCard/PriceCard.jsx
+++ src/components/PriceCard/PriceCard.jsx
@@ -5,7 +5,7 @@ const PriceCard = ({price}) => {
         <div>
             <h2>
             <span className='text-purple-500 text-5xl font-extrabold'>{price.price}</span>
-            <span className='text-2xl'>/mon</span>
+            <span className='text-2xl font-bold'>/mon</span>
             </h2>
             <h5 className='text-2xl font-bold'>{price.name}</h5>
         </div>
