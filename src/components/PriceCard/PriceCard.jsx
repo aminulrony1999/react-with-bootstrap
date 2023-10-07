5dbd96e79bcd9b0adf2b0ef65a05fe94620229f1
aminulrony1999 <aminulrony1999@gmail.com>
2023-10-07 19:11:16 +0600

    text centarized of the division

--- src/components/PriceCard/PriceCard.jsx
+++ src/components/PriceCard/PriceCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './PriceCard.css';
 const PriceCard = ({price}) => {
     return (
-        <div className='bg-indigo-400 mt-4 rounded-md p-4'>
+        <div className='bg-indigo-400 mt-4 rounded-md p-4 text-center'>
             <h2>
             <span className='text-purple-500 text-5xl font-extrabold'>{price.price}</span>
             <span className='text-2xl font-bold text-white'>/mon</span>
