b0d810455e8ae203ab9f08931dd2ace4ae051d5f
aminulrony1999 <aminulrony1999@gmail.com>
2023-10-07 18:45:56 +0600

    extra design added

--- src/components/PriceCard/PriceCard.jsx
+++ src/components/PriceCard/PriceCard.jsx
@@ -3,7 +3,7 @@ import './PriceCard.css';
 const PriceCard = ({price}) => {
     return (
         <div>
-            <h2 className='text-5xl'>{price.price}</h2>
+            <h2 className='text-5xl font-extrabold'>{price.price}</h2>
             <h5 className='text-2xl font-bold'>{price.name}</h5>
         </div>
     );
