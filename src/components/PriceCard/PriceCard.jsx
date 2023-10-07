01751881867bd5aa4ed60c32097f2ef3b4a1e168
aminulrony1999 <aminulrony1999@gmail.com>
2023-10-07 19:09:43 +0600

    some changes applied

--- src/components/PriceCard/PriceCard.jsx
+++ src/components/PriceCard/PriceCard.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import './PriceCard.css';
 const PriceCard = ({price}) => {
     return (
-        <div className='bg-indigo-400'>
+        <div className='bg-indigo-400 mt-4 rounded-md p-4'>
             <h2>
             <span className='text-purple-500 text-5xl font-extrabold'>{price.price}</span>
-            <span className='text-2xl font-bold'>/mon</span>
+            <span className='text-2xl font-bold text-white'>/mon</span>
             </h2>
             <h5 className='text-2xl font-bold'>{price.name}</h5>
         </div>
